87536e74ce3fd5f109a22dbfcf942578ca9972c8
parkour323923 <62733485+parkour323923@users.noreply.github.com>
2020-03-26 23:20:49 -0400

    Update Debugger.cs

--- Ryujinx.Graphics.OpenGL/Debugger.cs
+++ Ryujinx.Graphics.OpenGL/Debugger.cs
@@ -34,7 +34,7 @@ namespace Ryujinx.Graphics.OpenGL
             switch (type)
             {
                 case DebugType.DebugTypeError:
-                    Logger.PrintDebug(LogClass.Gpu, fullMessage);
+                    Logger.PrintError(LogClass.Gpu, fullMessage);
                     break;
                 case DebugType.DebugTypePerformance:
                     Logger.PrintWarning(LogClass.Gpu, fullMessage);
